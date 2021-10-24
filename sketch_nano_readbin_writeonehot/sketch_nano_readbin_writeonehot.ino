7871c4a906d4b2e2f2e14fabee0b88568bba01cc
Miles Alasdair Martin <miles.martin@maine.edu>
2021-10-24 17:40:39 -0400

    riveting progress, surely: added a few lines to assign input but also doing something else

--- sketch_nano_readbin_writeonehot/sketch_nano_readbin_writeonehot.ino
+++ sketch_nano_readbin_writeonehot/sketch_nano_readbin_writeonehot.ino
@@ -5,15 +5,20 @@ void setup() {
   int out_array[] = {1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
   // use to read binary input from UNO
   int in_array[] = {A4, A3, A2, A1}; 
-  int in_array_len[] = sizeof(in_array);
-  int out_array_len[] = sizeof(out_array);
+  
+  // find size for iteration/traversal
+  int out_array_len[] = sizeof(out_array)/sizeof(out_array[0]);
+  int in_array_len[] = sizeof(in_array)/sizeof(in_array[0]);
+  
+  // Set analog pins as digital inputs
   for (byte i = 0; i < in_array_len; i++) {
     pinMode(in_array[i], INPUT);
   }
+  // set up digital outputs
   for (byte i = 0; i < out_array_len; i++) {
     pinMode(out_array[i], OUTPUT);
   }
-
+  
 }
 
 void loop() {
