740abcc330575e11df3242f23b609d870962e7ab
Miles Alasdair Martin <miles.martin@maine.edu>
2021-11-15 15:53:39 -0500

    Updated to write all indices of matrix high; now that hardware issue resolved, works as intended

--- energia_tiva/write_pins_high/write_pins_high.ino
+++ energia_tiva/write_pins_high/write_pins_high.ino
@@ -1,18 +1,58 @@
-int out_row[15] = {40, 39, 38, 37, 26, 35, 34, 33, 32, 31, 15, 14, 13, 12, 11};
-int out_col[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-int out_row_len = 15;
-int out_col_len = 8;
+int rowPin[15] = {36, 35, 34, 33, 32, 31, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int colPin[8] = {19, 18, 17, 15, 14, 13, 12, 11};
+int rowPinLen = 15;
+int colPinLen = 8;
+int delayTime = 500;
 
 void setup() {
   // put your setup code here, to run once:
-  // for (int i = 0; i < out_row_len; i++) {
-    pinMode(40, OUTPUT);
-    digitalWrite(40, HIGH);
-    delay(1000);
-  for (int i = 0; i < out_col_len; i++) {
-    pinMode(out_col[i], OUTPUT);
-    digitalWrite(out_col[i], HIGH);
-    delay(1000);
+  
+  Serial.begin(9600);
+
+  for (int i = 0; i < rowPinLen; i++) {
+    Serial.print("Setting pin ");
+    Serial.print(rowPin[i]);
+    Serial.println(" to be LOW.");
+    pinMode(rowPin[i], OUTPUT);
+    digitalWrite(rowPin[i], LOW);
+//    delay(delayTime);
+    Serial.print("Wait on pin ");
+    Serial.print(rowPin[i]);
+    Serial.println(" complete.");
+  }
+  for (int i = 0; i < colPinLen; i++) {
+    Serial.print("Setting pin ");
+    Serial.print(colPin[i]);
+    Serial.println(" to be LOW.");
+    pinMode(colPin[i], OUTPUT);
+    digitalWrite(colPin[i], LOW);
+//    delay(delayTime);
+    Serial.print("Wait on pin ");
+    Serial.print(colPin[i]);
+    Serial.println(" complete.");
+  }
+  
+  for (int i = 0; i < rowPinLen; i++) {
+    Serial.print("Setting pin ");
+    Serial.print(rowPin[i]);
+    Serial.println(" to be HIGH.");
+    pinMode(rowPin[i], OUTPUT);
+    digitalWrite(rowPin[i], HIGH);
+    delay(delayTime);
+    Serial.print("Wait on pin ");
+    Serial.print(rowPin[i]);
+    Serial.println(" complete.");
+  }
+  for (int i = 0; i < colPinLen; i++) {
+    Serial.print("Setting pin ");
+    Serial.print(colPin[i]);
+    Serial.println(" to be HIGH.");
+    pinMode(colPin[i], OUTPUT);
+    digitalWrite(colPin[i], HIGH);
+    delay(delayTime);
+    Serial.print("Wait on pin ");
+    Serial.print(colPin[i]);
+    Serial.println(" complete.");
   }
 }
 
