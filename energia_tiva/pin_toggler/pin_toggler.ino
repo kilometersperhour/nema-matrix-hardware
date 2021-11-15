02a9ea1afa14679bb17f8e473ef2c36712041367
Miles Alasdair Martin <miles.martin@maine.edu>
2021-11-15 15:31:10 -0500

    Updating committed code for a baseline; breadboard troubleshooting revealed good software and problematic hardware

--- energia_tiva/pin_toggler/pin_toggler.ino
+++ energia_tiva/pin_toggler/pin_toggler.ino
@@ -20,8 +20,10 @@
 
 */
 
-/*
- * 
+/* Things to do:
+ * Create framebuffer manager
+ *   Set a row of pins according to the bytes set in a row
+ * Error checking
  */
 
 String inputString = ""; // a string to hold incoming data
@@ -30,30 +32,63 @@ boolean pinState = 1;
 int colSelect = 0;
 int rowSelect = 0;
 
+int rowPin[15] = {36, 35, 34, 33, 32, 31, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int colPin[8] = {19, 18, 17, 15, 14, 13, 12, 11};
+int rowPinLen = 15;
+int colPinLen = 8;
+
+boolean debugSerial = false; // enable = true; use to view string at different stages
+
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  // reserve 200 bytes for the inputString:
   inputString.reserve(200);
+
+  // Set all pins in use to low so they don't float
+  for (int i = 0; i < rowPinLen; i++) {
+    pinMode(rowPin[i], OUTPUT);
+    digitalWrite(rowPin[i], LOW);
+  }
+  for (int i = 0; i < colPinLen; i++) {
+    pinMode(colPin[i], OUTPUT);
+    digitalWrite(colPin[i], LOW);
+  }
+
+  for (int i = 0; i < rowPinLen; i++) {
+    pinMode(rowPin[i], OUTPUT);
+    digitalWrite(rowPin[i], HIGH);
+//    delay(50);
+  }
+  for (int i = 0; i < colPinLen; i++) {
+    pinMode(colPin[i], OUTPUT);
+    digitalWrite(colPin[i], HIGH);
+//    delay(50);
+  }
+
 }
 
 void loop() {
   // print the string when a newline arrives:
   if (stringComplete) {
     Serial.println(inputString); 
+
+    /* Vestigial debug code   
     // toggle appropriate pin
-    //if (!(inputString.startsWith("on ") || inputString.startsWith("off "))) {
-    //  Serial.println("Malformed command! Try again with 'on 0 1' to turn on LED at position (0,1), for example")
-    //} else {
-    //  while (inputString != "") {
-    //    lastCommand = inputString.lastIndexOf(' ');
-    //  }
+    if (!(inputString.startsWith("on ") || inputString.startsWith("off "))) {
+      Serial.println("Malformed command! Try again with 'on 0 1' to turn on LED at position (0,1), for example")
+    } else {
+      while (inputString != "") {
+        lastCommand = inputString.lastIndexOf(' ');
+      }
+    */
     
-    Serial.print("inputString before determining on/off = '");
-    Serial.print(inputString);
-    Serial.print("'");
-    Serial.println();    
+    if (debugSerial == true) {
+      Serial.print("inputString before determining on/off = '");
+      Serial.print(inputString);
+      Serial.print("'");
+      Serial.println();
+    }
     if (inputString.startsWith("of")) {
       pinState = 0;
     } else if (inputString.startsWith("on")) {
@@ -63,29 +98,35 @@ void loop() {
     inputString = inputString.substring(3); 
     
     // now string resembles '0 1x\n' where x is potentially a number in the ones place
-    Serial.print("inputString before determining column = '");
-    Serial.print(inputString);
-    Serial.print("'");
-    Serial.println();    
+    if (debugSerial == true) {
+      Serial.print("inputString before determining column = '");
+      Serial.print(inputString);
+      Serial.print("'");
+      Serial.println();    
+    }
     colSelect = inputString.charAt(0) - '0';
     inputString = inputString.substring(2);
 
     // now string resembles '1x\n' where x is potentially a number in the ones place
-    Serial.print("inputString before determining row = '");
-    Serial.print(inputString);
-    Serial.print("'");
-    Serial.println();    
+    if (debugSerial == true) {
+      Serial.print("inputString before determining row = '");
+      Serial.print(inputString);
+      Serial.print("'");
+      Serial.println();    
+    }
     if (inputString.charAt(1) == '\n') {
       rowSelect = inputString.charAt(0) - '0';
     } else {
       rowSelect = (10*(inputString.charAt(0)-'0') + (inputString.charAt(1)-'0'));
     }
     inputString = inputString.substring(1);
-    
-    Serial.print("inputString at end of iteration = '");
-    Serial.print(inputString);
-    Serial.print("'");
-    Serial.println();
+
+    if (debugSerial == true) { 
+      Serial.print("inputString at end of iteration = '");
+      Serial.print(inputString);
+      Serial.print("'");
+      Serial.println();
+    }
 
     Serial.print("Setting LED at (");
     Serial.print(colSelect);
@@ -97,8 +138,9 @@ void loop() {
 
     // Until pin configurator written, keep the following print statement
     // should prevent confusion about "why it doesn't work"
-    Serial.println("(Nothing has been configured, please write/enable pin configurator)");
-    //    Serial.println("Pin reconfigured.");
+    //Serial.println("(Nothing has been configured, please write/enable pin configurator)");
+    pinConfigurator(pinState,colSelect,rowSelect);
+    Serial.println("Pin reconfigured.");
 
     Serial.println();
     // clear the string:
@@ -127,9 +169,39 @@ void serialEvent() {
   }
 }
 
-void pinConfigurator () {
-  
+void pinConfigurator (int enable, int row, int col) {
+  static byte framebuffer[15] = {}; // all possible LEDs, one bit per LED, one byte per row
+  static byte prevRow; // possibly useful for efficiency
+//  static byte prevCol; // possibly useful for efficiency
+  framebuffer[0] = B11111111;
+  for (int i = 0; i < 8; i++) { // Disable column
+    digitalWrite(colPin[i], LOW);
+    //delay(1000); 
+  } 
+  digitalWrite(prevRow, LOW);                                // Disable row afterward
+  digitalWrite(rowPin[row], enable);                         // enable new row
+  // row-light-setting loop to enable new col lights. 7-0 grabs MSB and colPin[i] places it at (0,y)
+  for (int i = 0; i < 8; i++) { 
+    digitalWrite(colPin[i], (framebuffer[row] >> (7-i)) & 0x1);
+    //delay(1000);
+  }
 }
+
+/*void pinConfigurator (int enable, int row, int col) {
+  static byte framebuffer[15] = {}; // all possible LEDs, one bit per LED, one byte per row
+  static byte prevRow; // possibly useful for efficiency
+//  static byte prevCol; // possibly useful for efficiency
+  framebuffer = 
+  for (int i = 0; i < 8; i++) { // Disable column
+    digitalWrite(colPin[i], LOW); 
+  } 
+  digitalWrite(prevRow, LOW);                                // Disable row afterward
+  digitalWrite(rowPin[row], enable);                         // enable new row
+  // row-light-setting loop to enable new col lights. 7-0 grabs MSB and colPin[i] places it at (0,y)
+  for (int i = 0; i < 8; i++) { 
+    digitalWrite(colPin[i], (framebuffer[row] >> (7-i)) & 0x1);
+  }
+}*/
 
 // Error checking for later!! Get the functionality going first
 /*boolean containsSpaces(String buffer) {
