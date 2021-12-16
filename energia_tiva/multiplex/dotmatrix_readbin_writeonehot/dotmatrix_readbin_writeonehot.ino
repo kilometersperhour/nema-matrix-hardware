2bdce5cf23d5fc210d970715a21de0a38e526284
Miles Alasdair Martin <miles.martin@maine.edu>
2021-12-15 22:37:49 -0500

    Multiplexer code writes a '1' on the lower right display. Future work includes modifications to make framebuffer.

--- energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
+++ energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
@@ -1,43 +1,53 @@
 /*
- * To-dos: 
- *    Create framebuffer manager
- *    Error checking
- *    Move away from globals
- *    Change variables to smaller types
- *    Add better comments
- */
+   To-dos:
+      Create framebuffer manager
+      Error checking
+      Move away from globals
+      Change variables to smaller types
+      Add better comments
+*/
 
 boolean pinState = 1;
-int colSelect = 0;
-int rowSelect = 0;
+int matrixSelect = 0;
+int pixelSelect = 0;
+int pixelPin[22] = {28, 27, 26, 25, 2, 10, 8, 5, 31, 32, 33, 34, 35, 36, 37, 3, 4, 40, 11, 12, 13, 17};
+int matrixPin[4] = {14, 15, 18, 19};
+int pixelPinLen = 22;
+int matrixPinLen = 4;
 
-int rowPin[15] = {36, 35, 34, 33, 32, 31, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int colPin[8] = {19, 18, 17, 15, 14, 13, 12, 11};
-int rowPinLen = 15;
-int colPinLen = 8;
+int debug = 2;
+
+uint32_t matrixFramebufferMask[4] = {(B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111}; //
+uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                  ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                  ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                  ((B00000000 << 24) + (B00100001 << 16) + (B00100000 << 8) + B01001000), // one
+                                 };
 
 void setup() {
   // put your setup code here, to run once:
-
-  // Set all pins in use to low for initialization test
-  for (int i = 0; i < rowPinLen; i++) {
-    pinMode(rowPin[i], OUTPUT);
-    digitalWrite(rowPin[i], LOW);
+  if (debug) {
+    Serial.begin(1000000);
   }
-  for (int j = 0; j < colPinLen; j++) {
-    pinMode(colPin[j], OUTPUT);
-    digitalWrite(colPin[j], LOW);
+  // Set all pins in use to low for initialization test
+  for (int i = 0; i < pixelPinLen; i++) {
+    pinMode(pixelPin[i], OUTPUT);
+    digitalWrite(pixelPin[i], LOW);
+  }
+  for (int j = 0; j < matrixPinLen; j++) {
+    pinMode(matrixPin[j], OUTPUT);
+    digitalWrite(matrixPin[j], LOW);
   }
 }
 
-  
-int out_row[15] = {40, 39, 38, 37, 26, 35, 34, 33, 32, 31, 15, 14, 13, 12, 11};
-int out_col[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-int out_row_len = 15;
-int out_col_len = 8;
-int waittime = 60*1000/(out_row_len * out_col_len);
 
-// all possible LEDs, one bit per LED, one byte per row
+int out_pixel[15] = {40, 39, 38, 37, 26, 35, 34, 33, 32, 31, 15, 14, 13, 12, 11};
+int out_matrix[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+int out_pixel_len = 15;
+int out_matrix_len = 8;
+int waittime = 60 * 1000 / (out_pixel_len * out_matrix_len);
+
+// all possible LEDs, one bit per LED, one byte per pixel
 static byte framebuffer[15] =   {B00010001,
                                  B00100010,
                                  B00110011,
@@ -52,7 +62,8 @@ static byte framebuffer[15] =   {B00010001,
                                  B11001100,
                                  B11011101,
                                  B11101110,
-                                 B11111111};
+                                 B11111111
+                                };
 
 // ref: https://cdn.discordapp.com/attachments/882335631102079006/896048973012402226/image0.jpg
 uint32_t zero  = (B00000110 << 24) + (B10011001 << 16) + (B10011001 << 8) + B10010110; // little endian nybbles, L to R, most sig. to least sig.
@@ -67,40 +78,75 @@ uint32_t eight = (B00000110 << 24) + (B10011001 << 16) + (B01101001 << 8) + B100
 uint32_t nine  = (B00000110 << 24) + (B00010001 << 16) + (B01111001 << 8) + B10010110;
 
 uint8_t numberBinary[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-  
+
 uint32_t numberGlyphs[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
 uint8_t serialOutput;
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  //for (byte i = 0; i < out_row_len; i++) {
-  //  for (byte j = 0; j < out_col_len; j++) {
-  //    
-  //    }
-  //  }
-  for (int row = 0; row < 15; row++) {
-    static byte prevRow = 0; // possibly useful for efficiency
-  //  static byte prevCol; // possibly useful for efficiency
-  
-    for (int i = 0; i < 8; i++) { // Disable column
-      digitalWrite(colPin[i], LOW);
-      //delay(1000); 
-    } 
-    digitalWrite(rowPin[prevRow], LOW);                                // Disable row afterward
-    digitalWrite(rowPin[row], HIGH);                         // enable new row
-    // row-light-setting loop to enable new col lights. 7-0 grabs MSB and colPin[i] places it at (0,y)
-    for (int i = 0; i < 8; i++) { 
-      digitalWrite(colPin[i], (framebuffer[row] >> i) & 0x1);
-      //delay(1000);
+  // put your main code here, to run repeatedly:
+
+  while (1) { // big loop
+
+
+    for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
+      digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
+
+      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
+        digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
+      }
+
+      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
+        // This is a pixel-light-setting loop to enable new matrix lights.
+        // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
+        digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
+        if (debug>1) {
+          Serial.print("pixel ");
+          Serial.print(pixel);
+          Serial.print(" matrix ");
+          Serial.print(matrix);
+          Serial.print(" state ");
+          Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
+        }
+        
+      }
+      
+      delay(4);
+      digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
+      //delay(1);
     }
-    prevRow = row;
-    delayMicroseconds(1333);
+
   }
-  //delay(1);
+
+
+
+
+
+  /*  static byte prevPixel = 0; // possibly useful for efficiency
+    for (int pixel = 0; pixel < pixelPinLen; pixel++) {
+      //  static byte prevMatrix; // possibly useful for efficiency
+
+      for (int i = 0; i < matrixPinLen; i++) { // Disable matrix
+        digitalWrite(matrixPin[i], LOW);
+        //delay(1000);
+        digitalWrite(pixelPin[prevPixel], LOW); // Disable pixel afterward
+
+        digitalWrite(matrixPin[i], HIGH);    // enable matrix again
+        digitalWrite(pixelPin[pixel], HIGH);    // enable new
+
+
+
+        }
+
+
+      delay(50);
+      }
+    }
+
+    //delay(1);*/
 }
 
 void fillFramebuffer(uint32_t first, uint32_t second, uint32_t third, uint32_t fourth) {
 
-  
-  
+
+
 }
