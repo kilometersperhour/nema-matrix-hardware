7e296dc2d82eb743136e9308f4b079c52c96ad88
Miles Alasdair Martin <miles.martin@maine.edu>
2021-12-16 14:57:01 -0500

    Display updates with read from pixelSelect

--- energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
+++ energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
@@ -7,13 +7,17 @@
       Add better comments
 */
 
-boolean pinState = 1;
 int matrixSelect = 0;
-int pixelSelect = 0;
+int pixelSelect = 16;
 int pixelPin[22] = {28, 27, 26, 25, 2, 10, 8, 5, 31, 32, 33, 34, 35, 36, 37, 39, 38, 40, 11, 12, 13, 17};
 int matrixPin[4] = {14, 15, 18, 19};
 int pixelPinLen = 22;
 int matrixPinLen = 4;
+
+String inputString = ""; // a string to hold incoming data
+char outputString[100];
+boolean stringComplete = false; // whether the string is complete
+boolean pinState = 1;
 
 int debug = 0;
 
@@ -27,13 +31,7 @@ uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B111
 void setup() {
   // put your setup code here, to run once:
 
-  String inputString = ""; // a string to hold incoming data
-  boolean stringComplete = false; // whether the string is complete
-  boolean pinState = 1;
-  int matrixSelect = 0;
-  int pixelSelect = 0;
 
-  char outputString[100];
 
   Serial.begin(115200);
   // Set all pins in use to low for initialization test
@@ -70,17 +68,19 @@ void loop() {
     if (stringComplete) {
       Serial.println(inputString);
 
-      if (inputString.startsWith("of")) {
-        pinState = 0;
-      } else if (inputString.startsWith("on")) {
-        pinState = 1;
+      if (inputString.startsWith("+") || inputString.startsWith("-")) {
+        stringComplete = false;
+      } else {
+        stringComplete = true;
       }
-      // delete [of ] or [on ]
+      // delete [+] or [-] at beginning of string
       inputString = inputString.substring(3);
 
-      // now string resembles '0 1x\n' where x is potentially a number in the ones place
-      matrixSelect = inputString.charAt(0) - '0';
-      inputString = inputString.substring(2);
+      // now string resembles 'xx:yy\n' where x is potentially a number in the ones place
+      if (inputString.charAt(2) == ':') {
+        pixelSelect = (10 * (inputString.charAt(0) - '0') + (inputString.charAt(1) - '0'));
+      }
+      inputString = inputString.substring(3);
 
       // now string resembles '1x\n' where x is potentially a number in the ones place
       if (inputString.charAt(1) == '\n') {
@@ -88,22 +88,11 @@ void loop() {
       } else {
         pixelSelect = (10 * (inputString.charAt(0) - '0') + (inputString.charAt(1) - '0'));
       }
-      inputString = inputString.substring(1);
-      sprintf(outputString, "Setting LED at (%d,%d) to state %d.\n", matrixSelect, pixelSelect, pinState);
-      /*    Serial.print("Setting LED at (");
-          Serial.print(matrixSelect);
-          Serial.print(",");
-          Serial.print(pixelSelect);
-          Serial.print(") to state ");
-          Serial.print(pinState);
-          Serial.println(".");
-      */
-      Serial.println(outputString);
-      // Until pin configurator written, keep the following print statement
-      // should prevent confusion about "why it doesn't work"
-      //Serial.println("(Nothing has been configured, please write/enable pin configurator)");
-      pinConfigurator(pinState, matrixSelect, pixelSelect);
-      Serial.println("Pin reconfigured.");
+      inputString = inputString.substring(2);
+      if (debug) {
+        sprintf(outputString, "Setting LED at (%d,%d) to state %d.\n", matrixSelect, pixelSelect, pinState);
+        Serial.println(outputString);
+      }
 
       Serial.println();
       // clear the string:
@@ -121,23 +110,23 @@ void loop() {
         // This is a pixel-light-setting loop to enable new matrix lights.
         // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
         //          digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
-        digitalWrite(pixelPin[pixel], ((numberGlyphs[i] >> pixel) & 0x1));
+        digitalWrite(pixelPin[pixel], ((numberGlyphs[pixelSelect % 10] >> pixel) & 0x1));
         if (debug > 1) {
           Serial.print("pixel ");
           Serial.print(pixel);
           Serial.print(" matrix ");
           Serial.print(matrix);
           Serial.print(" state ");
-          Serial.print((numberGlyphs[i] >> pixel) & 0x1);
+          Serial.print((numberGlyphs[pixelSelect % 10] >> pixel) & 0x1);
           Serial.print(" numeral ");
-          //            Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
-          Serial.println(i);
+          Serial.println(pixelSelect % 10);
         }
 
       }
       delay(1000);
       digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
-      //delay(1);
+      Serial.println("Pin reconfigured.");
+
     }
 
   }
@@ -146,36 +135,13 @@ void loop() {
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read(); 
+    char inChar = (char)Serial.read();
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
-    stringComplete = true;
-    } 
+      stringComplete = true;
+    }
   }
-}
-
-void pinConfigurator (int enable, int matrix, int pixel) {
-//  static byte framebuffer[15] = {}; // all possible LEDs, one bit per LED, one byte per pixel
-//  static byte prevPixel; // possibly useful for efficiency
-//  static byte prevMatrix; // possibly useful for efficiency
-  Serial.println("ATTN: AUTO-OFF ENABLED");
-  for (int i = 0; i < matrixPinLen; i++) { // Disable matrix
-    digitalWrite(matrixPin[i], LOW);
-    //delay(1000); 
-  }
-  for (int i = 0; i < pixelPinLen; i++) { // Disable pixels per matrix
-    digitalWrite(pixelPin[i], LOW);
-    //delay(1000); 
-  } 
-  sprintf(outputString,"Running digitalWrite(%d, %d) given pixel = %d",pixelPin[pixel],enable,pixel);
-  Serial.println(outputString);
-  digitalWrite(pixelPin[pixel], enable);                         // enable new pixel
-  sprintf(outputString,"Running digitalWrite(%d, %d) given matrix = %d",matrixPin[matrix],enable,matrix);
-  Serial.println(outputString);
-  digitalWrite(matrixPin[matrix], enable);                           // enable new matrix
-
-  // pixel-light-setting loop to enable new matrix lights. 7-0 grabs MSB and matrixPin[i] places it at (0,y)
 }
