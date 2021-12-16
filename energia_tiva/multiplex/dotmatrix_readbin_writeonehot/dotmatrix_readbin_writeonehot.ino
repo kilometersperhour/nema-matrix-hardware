048a1f60d31ae577623ec5d3a9e6133036a9d03b
Miles Alasdair Martin <miles.martin@maine.edu>
2021-12-16 14:30:49 -0500

    Added helper functions from serial reading script

--- energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
+++ energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
@@ -26,9 +26,16 @@ uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B111
 
 void setup() {
   // put your setup code here, to run once:
-  if (debug) {
-    Serial.begin(115200);
-  }
+
+  String inputString = ""; // a string to hold incoming data
+  boolean stringComplete = false; // whether the string is complete
+  boolean pinState = 1;
+  int matrixSelect = 0;
+  int pixelSelect = 0;
+
+  char outputString[100];
+
+  Serial.begin(115200);
   // Set all pins in use to low for initialization test
   for (int i = 0; i < pixelPinLen; i++) {
     pinMode(pixelPin[i], OUTPUT);
@@ -60,71 +67,115 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   while (1) { // big loop
+    if (stringComplete) {
+      Serial.println(inputString);
 
-    for (int i = 0; i < 10; i++) { // index max of 10 because there are 10 glyphs
-      for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
-        digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
-
-        for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
-          digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
-        }
-
-        for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
-          // This is a pixel-light-setting loop to enable new matrix lights.
-          // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
-//          digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
-          digitalWrite(pixelPin[pixel], ((numberGlyphs[i] >> pixel) & 0x1));
-          if (debug > 1) {
-            Serial.print("pixel ");
-            Serial.print(pixel);
-            Serial.print(" matrix ");
-            Serial.print(matrix);
-            Serial.print(" state ");
-            Serial.print((numberGlyphs[i] >> pixel) & 0x1);
-            Serial.print(" numeral ");
-//            Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
-            Serial.println(i);
-          }
-
-        }
-        delay(1000);
-        digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
-        //delay(1);
+      if (inputString.startsWith("of")) {
+        pinState = 0;
+      } else if (inputString.startsWith("on")) {
+        pinState = 1;
       }
+      // delete [of ] or [on ]
+      inputString = inputString.substring(3);
+
+      // now string resembles '0 1x\n' where x is potentially a number in the ones place
+      matrixSelect = inputString.charAt(0) - '0';
+      inputString = inputString.substring(2);
+
+      // now string resembles '1x\n' where x is potentially a number in the ones place
+      if (inputString.charAt(1) == '\n') {
+        pixelSelect = inputString.charAt(0) - '0';
+      } else {
+        pixelSelect = (10 * (inputString.charAt(0) - '0') + (inputString.charAt(1) - '0'));
+      }
+      inputString = inputString.substring(1);
+      sprintf(outputString, "Setting LED at (%d,%d) to state %d.\n", matrixSelect, pixelSelect, pinState);
+      /*    Serial.print("Setting LED at (");
+          Serial.print(matrixSelect);
+          Serial.print(",");
+          Serial.print(pixelSelect);
+          Serial.print(") to state ");
+          Serial.print(pinState);
+          Serial.println(".");
+      */
+      Serial.println(outputString);
+      // Until pin configurator written, keep the following print statement
+      // should prevent confusion about "why it doesn't work"
+      //Serial.println("(Nothing has been configured, please write/enable pin configurator)");
+      pinConfigurator(pinState, matrixSelect, pixelSelect);
+      Serial.println("Pin reconfigured.");
+
+      Serial.println();
+      // clear the string:
+      inputString = "";
+      stringComplete = false;
+    }
+    for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
+      digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
+
+      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
+        digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
+      }
+
+      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
+        // This is a pixel-light-setting loop to enable new matrix lights.
+        // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
+        //          digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
+        digitalWrite(pixelPin[pixel], ((numberGlyphs[i] >> pixel) & 0x1));
+        if (debug > 1) {
+          Serial.print("pixel ");
+          Serial.print(pixel);
+          Serial.print(" matrix ");
+          Serial.print(matrix);
+          Serial.print(" state ");
+          Serial.print((numberGlyphs[i] >> pixel) & 0x1);
+          Serial.print(" numeral ");
+          //            Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
+          Serial.println(i);
+        }
+
+      }
+      delay(1000);
+      digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
+      //delay(1);
     }
 
   }
-
-
-
-
-
-  /*  static byte prevPixel = 0; // possibly useful for efficiency
-    for (int pixel = 0; pixel < pixelPinLen; pixel++) {
-      //  static byte prevMatrix; // possibly useful for efficiency
-
-      for (int i = 0; i < matrixPinLen; i++) { // Disable matrix
-        digitalWrite(matrixPin[i], LOW);
-        //delay(1000);
-        digitalWrite(pixelPin[prevPixel], LOW); // Disable pixel afterward
-
-        digitalWrite(matrixPin[i], HIGH);    // enable matrix again
-        digitalWrite(pixelPin[pixel], HIGH);    // enable new
-
-
-
-        }
-
-
-      delay(50);
-      }
-    }
-
-    //delay(1);*/
 }
 
-void fillFramebuffer(uint32_t first, uint32_t second, uint32_t third, uint32_t fourth) {
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+    stringComplete = true;
+    } 
+  }
+}
 
+void pinConfigurator (int enable, int matrix, int pixel) {
+//  static byte framebuffer[15] = {}; // all possible LEDs, one bit per LED, one byte per pixel
+//  static byte prevPixel; // possibly useful for efficiency
+//  static byte prevMatrix; // possibly useful for efficiency
+  Serial.println("ATTN: AUTO-OFF ENABLED");
+  for (int i = 0; i < matrixPinLen; i++) { // Disable matrix
+    digitalWrite(matrixPin[i], LOW);
+    //delay(1000); 
+  }
+  for (int i = 0; i < pixelPinLen; i++) { // Disable pixels per matrix
+    digitalWrite(pixelPin[i], LOW);
+    //delay(1000); 
+  } 
+  sprintf(outputString,"Running digitalWrite(%d, %d) given pixel = %d",pixelPin[pixel],enable,pixel);
+  Serial.println(outputString);
+  digitalWrite(pixelPin[pixel], enable);                         // enable new pixel
+  sprintf(outputString,"Running digitalWrite(%d, %d) given matrix = %d",matrixPin[matrix],enable,matrix);
+  Serial.println(outputString);
+  digitalWrite(matrixPin[matrix], enable);                           // enable new matrix
 
-
+  // pixel-light-setting loop to enable new matrix lights. 7-0 grabs MSB and matrixPin[i] places it at (0,y)
 }
