71a313180fab12f4b6594cc7aba491e40f42d7b5
Miles Alasdair Martin <miles.martin@maine.edu>
2021-12-16 13:51:21 -0500

    Glyphs repacked from 28-bit form to 22-bit form. Need more work to reassign UART pins

--- energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
+++ energia_tiva/multiplex/dotmatrix_readbin_writeonehot/dotmatrix_readbin_writeonehot.ino
@@ -15,7 +15,7 @@ int matrixPin[4] = {14, 15, 18, 19};
 int pixelPinLen = 22;
 int matrixPinLen = 4;
 
-int debug = 2;
+int debug = 0;
 
 uint32_t matrixFramebufferMask[4] = {(B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111}; //
 uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
@@ -27,7 +27,7 @@ uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B111
 void setup() {
   // put your setup code here, to run once:
   if (debug) {
-    Serial.begin(1000000);
+    Serial.begin(57600);
   }
   // Set all pins in use to low for initialization test
   for (int i = 0; i < pixelPinLen; i++) {
@@ -43,16 +43,16 @@ void setup() {
 int waittime = 4; // 4ms = <62.5Hz/display, 25% duty cycle
 
 // ref: https://cdn.discordapp.com/attachments/882335631102079006/896048973012402226/image0.jpg
-uint32_t zero  = (B00000110 << 24) + (B10011001 << 16) + (B10011001 << 8) + B10010110; // little endian nybbles, L to R, most sig. to least sig.
-uint32_t one   = (B00000010 << 24) + (B00100010 << 16) + (B00100010 << 8) + B00100010;
-uint32_t two   = (B00001111 << 24) + (B10000100 << 16) + (B00100001 << 8) + B10010110;
-uint32_t three = (B00000110 << 24) + (B10010001 << 16) + (B00100001 << 8) + B10010110;
-uint32_t four  = (B00000001 << 24) + (B00011111 << 16) + (B10010101 << 8) + B00110001;
-uint32_t five  = (B00000110 << 24) + (B10010001 << 16) + (B00011110 << 8) + B10001111;
-uint32_t six   = (B00000110 << 24) + (B10011001 << 16) + (B11101000 << 8) + B10000110;
-uint32_t seven = (B00001000 << 24) + (B10000100 << 16) + (B00100001 << 8) + B00011111;
-uint32_t eight = (B00000110 << 24) + (B10011001 << 16) + (B01101001 << 8) + B10010110;
-uint32_t nine  = (B00000110 << 24) + (B00010001 << 16) + (B01111001 << 8) + B10010110;
+uint32_t zero = (B00000000 << 24) + (B00010011 << 16) + (B01100100 << 8) + B11010110;
+uint32_t one = (B00000000 << 24) + (B00010100 << 16) + (B00010010 << 8) + B00100100;
+uint32_t two = (B00000000 << 24) + (B00111010 << 16) + (B10010000 << 8) + B01010110;
+uint32_t three = (B00000000 << 24) + (B00010011 << 16) + (B00010000 << 8) + B01010110;
+uint32_t four = (B00000000 << 24) + (B00100001 << 16) + (B11100101 << 8) + B01101000;
+uint32_t five = (B00000000 << 24) + (B00010011 << 16) + (B00100011 << 8) + B10011111;
+uint32_t six = (B00000000 << 24) + (B00010011 << 16) + (B01011100 << 8) + B10010110;
+uint32_t seven = (B00000000 << 24) + (B00001010 << 16) + (B10010000 << 8) + B01001111;
+uint32_t eight = (B00000000 << 24) + (B00010011 << 16) + (B01011000 << 8) + B11010110;
+uint32_t nine = (B00000000 << 24) + (B00010001 << 16) + (B00111000 << 8) + B11010110;
 
 uint32_t numberGlyphs[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
 
@@ -61,32 +61,36 @@ void loop() {
 
   while (1) { // big loop
 
+    for (int i = 0; i < 10; i++) { // index max of 10 because there are 10 glyphs
+      for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
+        digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
 
-    for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
-      digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
-
-      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
-        digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
-      }
-
-      for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
-        // This is a pixel-light-setting loop to enable new matrix lights.
-        // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
-        digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
-        if (debug>1) {
-          Serial.print("pixel ");
-          Serial.print(pixel);
-          Serial.print(" matrix ");
-          Serial.print(matrix);
-          Serial.print(" state ");
-          Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
+        for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
+          digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
         }
-        
+
+        for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
+          // This is a pixel-light-setting loop to enable new matrix lights.
+          // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
+//          digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
+          digitalWrite(pixelPin[pixel], ((numberGlyphs[i] >> pixel) & 0x1));
+          if (debug > 1) {
+            Serial.print("pixel ");
+            Serial.print(pixel);
+            Serial.print(" matrix ");
+            Serial.print(matrix);
+            Serial.print(" state ");
+            Serial.print((numberGlyphs[i] >> pixel) & 0x1);
+            Serial.print(" numeral ");
+//            Serial.println((matrixFramebuffer[matrix] >> pixel) & 0x1);
+            Serial.println(i);
+          }
+
+        }
+        delay(1000);
+        digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
+        //delay(1);
       }
-      
-      delay(4);
-      digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
-      //delay(1);
     }
 
   }
