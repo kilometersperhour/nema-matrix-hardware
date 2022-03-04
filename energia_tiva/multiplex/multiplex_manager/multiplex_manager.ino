b18658965778420098e108c333ff0c3260c44683
Miles Alasdair Martin <miles.martin@maine.edu>
2022-03-03 20:54:25 -0500

    Syncing code from personal laptop to use on Raspberry Pi

--- energia_tiva/multiplex/multiplex_manager/multiplex_manager.ino
+++ energia_tiva/multiplex/multiplex_manager/multiplex_manager.ino
@@ -5,14 +5,15 @@
       Move away from globals
       Change variables to smaller types
       Add better comments
+      Make functions
 */
 
 int matrixSelect = 0;
 int pixelSelect = 17;
-int pixelPin[22] = {28, 27, 26, 25, 2, 10, 8, 5, 31, 32, 33, 34, 35, 36, 37, 39, 38, 40, 11, 12, 13, 17};
-int matrixPin[4] = {14, 15, 18, 19};
-int pixelPinLen = 22;
-int matrixPinLen = 4;
+const int pixelPin[22] = {28, 27, 26, 25, 2, 10, 8, 5, 31, 32, 33, 34, 35, 36, 37, 39, 38, 40, 11, 12, 13, 17};
+const int matrixPin[4] = {14, 15, 18, 19};
+const int pixelPinLen = 22;
+const int matrixPinLen = 4;
 
 String inputString = ""; // a string to hold incoming data
 char outputString[100];
@@ -27,22 +28,17 @@ uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B111
                                   ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
                                   ((B00000000 << 24) + (B00100001 << 16) + (B00100000 << 8) + B01001000), // one
                                  };
+void setMuxingPinsLow();
+
+void parseTimeString();
 
 void setup() {
   // put your setup code here, to run once:
 
-
-
   Serial.begin(115200);
   // Set all pins in use to low for initialization test
-  for (int i = 0; i < pixelPinLen; i++) {
-    pinMode(pixelPin[i], OUTPUT);
-    digitalWrite(pixelPin[i], LOW);
-  }
-  for (int j = 0; j < matrixPinLen; j++) {
-    pinMode(matrixPin[j], OUTPUT);
-    digitalWrite(matrixPin[j], LOW);
-  }
+  setMuxingPinsLow();
+
 }
 
 int waittime = 4; // 4ms = <62.5Hz/display, 25% duty cycle
@@ -61,12 +57,14 @@ uint32_t nine = (B00000000 << 24) + (B00010001 << 16) + (B00111000 << 8) + B1101
 
 uint32_t numberGlyphs[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
 
+
+
 void loop() {
   // put your main code here, to run repeatedly:
 
   while (1) { // big loop
     if (stringComplete) {
-      Serial.println(inputString);
+      Serial.println(inputString); // 
 
       if (inputString.startsWith("+") || inputString.startsWith("-")) {
         stringComplete = false;
@@ -99,7 +97,7 @@ void loop() {
       inputString = "";
       stringComplete = false;
     }
-    for (int matrix = 3; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
+    for (int matrix = 0; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
       digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
 
       for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
@@ -107,11 +105,23 @@ void loop() {
       }
 
       for (int i = 0; i < 10; i++) {
+        delayMicroseconds(waittime*100);//delay(1);
         for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
           // This is a pixel-light-setting loop to enable new matrix lights.
           // (7-0) grabs MSB and matrixPin[i] places it at (0,y), for example
           //          digitalWrite(pixelPin[pixel], (matrixFramebuffer[matrix] >> pixel) & 0x1);
-          digitalWrite(pixelPin[pixel], ((numberGlyphs[i] >> pixel) & 0x1));
+          if (matrix == 0) {
+            digitalWrite(pixelPin[pixel], ((numberGlyphs[1] >> pixel) & 0x1));
+          }
+          if (matrix == 1) {
+            digitalWrite(pixelPin[pixel], ((numberGlyphs[3] >> pixel) & 0x1));
+          }
+          if (matrix == 2) {
+            digitalWrite(pixelPin[pixel], ((numberGlyphs[3] >> pixel) & 0x1));
+          }
+          if (matrix == 3) {
+            digitalWrite(pixelPin[pixel], ((numberGlyphs[7] >> pixel) & 0x1));
+          }
           if (debug > 1) {
             Serial.print("pixel ");
             Serial.print(pixel);
@@ -120,17 +130,21 @@ void loop() {
             Serial.print(" state ");
             Serial.print((numberGlyphs[pixelSelect % 10] >> pixel) & 0x1);
             Serial.print(" numeral ");
-            Serial.println(pixelSelect % 10);
+            Serial.println(i);
             
       
           }
           
         }
-        delay(1000);`
-      }
+        //delayMicroseconds(waittime*100);
+      }      
       digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
-      Serial.println("Pin reconfigured.");
-
+      
+      if (debug > 0) {
+        Serial.print("Pin ");
+        Serial.print(matrix);
+        Serial.println(" reconfigured.");
+      }
     }
 
   }
@@ -148,4 +162,17 @@ void serialEvent() {
       stringComplete = true;
     }
   }
+}
+
+void setMuxingPinsLow() {
+
+  for (int i = 0; i < pixelPinLen; i++) {
+    pinMode(pixelPin[i], OUTPUT);
+    digitalWrite(pixelPin[i], LOW);
+  }
+  for (int j = 0; j < matrixPinLen; j++) {
+    pinMode(matrixPin[j], OUTPUT);
+    digitalWrite(matrixPin[j], LOW);
+  }
+  
 }
