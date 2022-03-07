7ed0ee8a875d2cfe31573a54345d238dffb10b51
Miles Alasdair Martin <miles.martin@maine.edu>
2022-03-07 11:03:45 -0500

    Additional work in scratchpad 1, trying to make functions from sheet of code

--- energia_tiva/multiplex/multiplex_manager/multiplex_manager.ino
+++ energia_tiva/multiplex/multiplex_manager/multiplex_manager.ino
@@ -12,8 +12,8 @@ int matrixSelect = 0;
 int pixelSelect = 17;
 const int pixelPin[22] = {28, 27, 26, 25, 2, 10, 8, 5, 31, 32, 33, 34, 35, 36, 37, 39, 38, 40, 11, 12, 13, 17};
 const int matrixPin[4] = {14, 15, 18, 19};
-const int pixelPinLen = 22;
-const int matrixPinLen = 4;
+const int pixelPinLen = sizeof(pixelPin[])/sizeof(pixelPin[0]); //22;
+const int matrixPinLen = sizeof(matrixPin[])/sizeof(matrixPin[0]); //4;
 
 String inputString = ""; // a string to hold incoming data
 char outputString[100];
@@ -22,15 +22,11 @@ boolean pinState = 1;
 
 int debug = 0;
 
-uint32_t matrixFramebufferMask[4] = {(B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111}; //
-uint32_t matrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
-                                  ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
-                                  ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
-                                  ((B00000000 << 24) + (B00100001 << 16) + (B00100000 << 8) + B01001000), // one
-                                 };
 void setMuxingPinsLow();
 
 void parseTimeString();
+
+void writeNumeralsAll(int h10, int h01, int m10, int m01);
 
 void setup() {
   // put your setup code here, to run once:
@@ -44,16 +40,16 @@ void setup() {
 int waittime = 4; // 4ms = <62.5Hz/display, 25% duty cycle
 
 // ref: https://cdn.discordapp.com/attachments/882335631102079006/896048973012402226/image0.jpg
-uint32_t zero = (B00000000 << 24) + (B00010011 << 16) + (B01100100 << 8) + B11010110;
-uint32_t one = (B00000000 << 24) + (B00010100 << 16) + (B00010010 << 8) + B00100100;
-uint32_t two = (B00000000 << 24) + (B00111010 << 16) + (B10010000 << 8) + B01010110;
-uint32_t three = (B00000000 << 24) + (B00010011 << 16) + (B00010000 << 8) + B01010110;
-uint32_t four = (B00000000 << 24) + (B00100001 << 16) + (B11100101 << 8) + B01101000;
-uint32_t five = (B00000000 << 24) + (B00010011 << 16) + (B00100011 << 8) + B10011111;
-uint32_t six = (B00000000 << 24) + (B00010011 << 16) + (B01011100 << 8) + B10010110;
-uint32_t seven = (B00000000 << 24) + (B00001010 << 16) + (B10010000 << 8) + B01001111;
-uint32_t eight = (B00000000 << 24) + (B00010011 << 16) + (B01011000 << 8) + B11010110;
-uint32_t nine = (B00000000 << 24) + (B00010001 << 16) + (B00111000 << 8) + B11010110;
+const uint32_t zero = (B00000000 << 24) + (B00010011 << 16) + (B01100100 << 8) + B11010110;
+const uint32_t one = (B00000000 << 24) + (B00010100 << 16) + (B00010010 << 8) + B00100100;
+const uint32_t two = (B00000000 << 24) + (B00111010 << 16) + (B10010000 << 8) + B01010110;
+const uint32_t three = (B00000000 << 24) + (B00010011 << 16) + (B00010000 << 8) + B01010110;
+const uint32_t four = (B00000000 << 24) + (B00100001 << 16) + (B11100101 << 8) + B01101000;
+const uint32_t five = (B00000000 << 24) + (B00010011 << 16) + (B00100011 << 8) + B10011111;
+const uint32_t six = (B00000000 << 24) + (B00010011 << 16) + (B01011100 << 8) + B10010110;
+const uint32_t seven = (B00000000 << 24) + (B00001010 << 16) + (B10010000 << 8) + B01001111;
+const uint32_t eight = (B00000000 << 24) + (B00010011 << 16) + (B01011000 << 8) + B11010110;
+const uint32_t nine = (B00000000 << 24) + (B00010001 << 16) + (B00111000 << 8) + B11010110;
 
 uint32_t numberGlyphs[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
 
@@ -61,6 +57,16 @@ uint32_t numberGlyphs[10] = {zero, one, two, three, four, five, six, seven, eigh
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+//For testing purposes only, for use in debug writers
+const uint32_t debugMatrixFramebufferMask[4] = {(B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111}; //
+const uint32_t debugMatrixFramebuffer[4] =  {((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                        ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                        ((B00000000 << 24) + (B00111111 << 16) + (B11111111 << 8) + B11111111),
+                                        ((B00000000 << 24) + (B00100001 << 16) + (B00100000 << 8) + B01001000), // one
+                                 };
+*/
 
   while (1) { // big loop
     if (stringComplete) {
@@ -97,13 +103,13 @@ void loop() {
       inputString = "";
       stringComplete = false;
     }
-    for (int matrix = 0; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
+/*    for (int matrix = 0; matrix < matrixPinLen; matrix++) { // per-matrix drawing loop
       digitalWrite(matrixPin[matrix], HIGH); // disable the current matrix, comment out to watch pixels blink out
 
       for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel deactivation loop
         digitalWrite(pixelPin[pixel], LOW); // Disable pixel by pixel, clean slate
       }
-
+      // make function: void writeNumeralsAll(int select, int h10, int h01, int m10, int m01); 
       for (int i = 0; i < 10; i++) {
         delayMicroseconds(waittime*100);//delay(1);
         for (int pixel = 0; pixel < pixelPinLen; pixel++) { // per-pixel drawing loop
@@ -130,14 +136,11 @@ void loop() {
             Serial.print(" state ");
             Serial.print((numberGlyphs[pixelSelect % 10] >> pixel) & 0x1);
             Serial.print(" numeral ");
-            Serial.println(i);
-            
-      
-          }
-          
+            Serial.println(i);      
+          } 
         }
-        //delayMicroseconds(waittime*100);
       }      
+
       digitalWrite(matrixPin[matrix], LOW); // disable the current matrix, comment out to watch pixels blink out
       
       if (debug > 0) {
@@ -146,7 +149,7 @@ void loop() {
         Serial.println(" reconfigured.");
       }
     }
-
+*/
   }
 }
 
@@ -174,5 +177,21 @@ void setMuxingPinsLow() {
     pinMode(matrixPin[j], OUTPUT);
     digitalWrite(matrixPin[j], LOW);
   }
+  
+}
+
+void writeNumeral(int selMatrix, int numGlyph){
+  digitalWrite(pixelPin[pixel], ((numberGlyphs[numGlyph] >> pixel) & 0x1));
+}
+          if (debug > 1) {
+            Serial.print("pixel ");
+            Serial.print(pixel);
+            Serial.print(" matrix ");
+            Serial.print(matrix);
+            Serial.print(" state ");
+            Serial.print((numberGlyphs[pixelSelect % 10] >> pixel) & 0x1);
+            Serial.print(" numeral ");
+            Serial.println(i);      
+          } 
   
 }
